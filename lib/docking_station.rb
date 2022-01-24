4567eaed6adc6b5a6f009eecbd4863d39b7deb3e
Mark <markjpoole@yahoo.com>
2022-01-24 22:34:53 +0000

    refactored DockingStation by extracting to private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,18 +4,30 @@ class DockingStation
 
   attr_reader :bikes
 
+  Capacity = 20
+
   def initialize
     @bikes = []
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bikes.count >= 20
+    fail 'Docking station is full' if fall?
     @bikes << bike
+  end
+
+  private
+
+  def fall?
+    @bikes.count >= Capacity
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
